17754e19608e197443bc8ba4a741527b1bcb3719
Jobit Joseph <51044706+jobitjoseph@users.noreply.github.com>
2023-02-03 19:51:43 +0530

    Delete ch55xduino directory

--- ch55xduino/ch55x/cores/ch55xduino/weakOverride.c
+++
@@ -1,8 +0,0 @@
-//SDCC doesn't support weak attribute. But function in sketch can override function in library.
-//Arduino compiles core as an archive and linked with the sketch
-
-#ifdef USER_USB_RAM
-void USBInterrupt(void){
-    //weak
-}
-#endif
